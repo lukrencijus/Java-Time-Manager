c97bdc700c1b6a1046064155f8357d7331d4190a
Zabuelis <karoliszabulis@gmail.com>
2024-05-01 19:40:12 +0300

    Documentation update

--- src/App.java
+++ src/App.java
@@ -1,3 +1,11 @@
+//This project was made by Karolis Zabulis and Lukas Šerelis.
+//Vilnius University Mathematics and Informatics faculty.
+//Contact: karolis.zabulis@mif.stud.vu.lt or lukas.serelis@mif.stud.vu.lt
+//Description of the program: A Java program that allows users to enter activity,
+//date, start, end of it, interruptions, comments and represents it.
+//Version: V0.1
+//Change log:
+//Program was tested on Windows 11 64 bit and macOS arm64
 public class App {
     public static void main(String[] args) throws Exception {
         new GUI();
